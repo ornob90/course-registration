bf9e9880a0b570dc5b305fcafb4f3cad2573522f
ornob90 <oornob49@gmail.com>
2023-09-15 07:07:13 +0600

    dynamic cart section with state handle complete

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,20 +4,19 @@ const Cart = ({ totalPrice, totalCredit, cart }) => {
   return (
     <div className="h-max pb-8 text-[#1c1b1b99] flex flex-col gap-3 w-full md:w-2/4 lg:w-[25%] bg-white rounded-lg p-3">
       <h3 className="text-[#2F80ED] font-bold text-lg">
-        Credit Hour Remaining 20hr
+        Credit Hour Remaining {20 - totalCredit}hr
       </h3>
       <hr />
       <h2 className="text-xl font-bold text-black">Course Name</h2>
       <ol className="pl-5 list-decimal ">
-        {/* {cart.map(({course_name}, idx) => (
-                <li key={idx}>{course_name}</li>
-            ))} */}
-        <li>1. Something</li>
+        {cart.map(({ course_name }, idx) => (
+          <li key={idx}>{course_name}</li>
+        ))}
       </ol>
       <hr />
-      <p className="font-medium ">Total Credit Hour: 1hr</p>
+      <p className="font-medium ">Total Credit Hour: {totalCredit}hr</p>
       <hr />
-      <p className="font-medium">Total Price: 2USD</p>
+      <p className="font-medium">Total Price: {totalPrice} USD</p>
     </div>
   );
 };
